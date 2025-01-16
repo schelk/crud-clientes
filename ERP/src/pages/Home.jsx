a713cfe25e7807f176ff08116cdb6b57dfe6a8b0
schelk <isaacfschelk@outlook.com>
2025-01-16 17:54:05 -0300

    adjust server

--- ERP/src/pages/Home.jsx
+++ ERP/src/pages/Home.jsx
@@ -1,17 +1,36 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
   const [backData, setbackData] = useState([{}]);
+  const [array, setarray] = useState([]);
+  const fetchApi = async () => {
+    const response = await axios.get("http://localhost:8080/api");
+    setarray(response.data.fruits);
+    console.log(response.data.fruits);
+  };
 
   useEffect(() => {
-    first;
-
-    return () => {
-      second;
-    };
+    fetchApi();
   }, []);
 
-  return <div></div>;
+  // useEffect(() => {
+  //   fetch("/api")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setbackData(data);
+  //     });
+  // }, []);
+
+  return (
+    <div>
+      <ul>
+        {array.map((f) => (
+          <li>{f}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
--- back/server.js
+++ back/server.js
@@ -1,43 +1,26 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const app = express();
-const port = 3000;
-// Open SQLite database (it creates the file if it doesn't exist)
-const db = new sqlite3.Database("./mydatabase.db");
+const cors = require("cors");
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+};
 
-// Endpoint to create a table
+app.use(cors(corsOptions));
+
+// Open SQLite database (it creates the file if it doesn't exist)
+const db = new sqlite3.Database(
+  "./mydatabase.db",
+  sqlite3.OPEN_READWRITE,
+  (err) => {
+    if (err) return console.error(err.message);
+  }
+);
+
 app.get("/api", (req, res) => {
-  res.json({ user: ["userOne", "userTwo", "userThree"] });
+  res.json({ fruits: ["server", "is", "working"] });
 });
 
-// // Endpoint to insert data into the table
-// app.get("/add", (req, res) => {
-//   const { name, age } = req.query;
-//   db.run(
-//     "INSERT INTO users (name, age) VALUES (?, ?)",
-//     [name, age],
-//     function (err) {
-//       if (err) {
-//         res.status(500).send("Error inserting data: " + err.message);
-//       } else {
-//         res.send(`Added user with ID ${this.lastID}`);
-//       }
-//     }
-//   );
-// });
-
-// // Endpoint to fetch all users
-// app.get("/users", (req, res) => {
-//   db.all("SELECT * FROM users", [], (err, rows) => {
-//     if (err) {
-//       res.status(500).send("Error fetching users: " + err.message);
-//     } else {
-//       res.json(rows);
-//     }
-//   });
-// });
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(8080, () => {
+  console.log("Server running at http://localhost:8080");
 });
